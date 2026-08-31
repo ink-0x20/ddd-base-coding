65c3f251a6e686a90bee3f1760e1768a7fd0f3a9
ink.0x20 <ink.0x20@gmail.com>
2026-09-01 00:02:07 +0900

    feat: ユーザーDTOを追加

---
+++ src/main/java/com/inkblogdb/ddd/application/dto/user/UserDTO.java
@@ -0,0 +1,13 @@
+package com.inkblogdb.ddd.application.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserDTO {
+
+  private String id;
+  private String name;
+
+}

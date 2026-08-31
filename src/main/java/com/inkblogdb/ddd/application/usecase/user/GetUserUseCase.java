c8072d8afa101a99803caf666b1b46b7f91100a9
ink.0x20 <ink.0x20@gmail.com>
2026-09-01 00:15:34 +0900

    feat: ユーザー取得ユースケース実装

---
+++ src/main/java/com/inkblogdb/ddd/application/usecase/user/GetUserUseCase.java
@@ -0,0 +1,30 @@
+package com.inkblogdb.ddd.application.usecase.user;
+
+import com.inkblogdb.ddd.application.dto.user.UserDTO;
+import com.inkblogdb.ddd.application.usecase.abort.UserNotFondException;
+import com.inkblogdb.ddd.domain.model.user.User;
+import com.inkblogdb.ddd.domain.model.user.UserId;
+import com.inkblogdb.ddd.domain.model.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class GetUserUseCase {
+
+  private final UserRepository userRepository;
+
+  public UserDTO getUser(UserId userId) throws UserNotFondException {
+    Optional<User> user = userRepository.findById(userId);
+    if (user.isEmpty()) {
+      throw new UserNotFondException("ユーザーが見つかりません");
+    }
+    return new UserDTO(
+        user.get().userId().value()
+        , user.get().userName().value()
+    );
+  }
+
+}

77b3f7c1d6c6fc139a7e6afc44810ac5efbd0a59
ink.0x20 <ink.0x20@gmail.com>
2026-09-02 00:10:52 +0900

    feat: ユーザーを追加するユースケースを実装

---
+++ src/main/java/com/inkblogdb/ddd/application/usecase/user/AddUserUseCase.java
@@ -0,0 +1,23 @@
+package com.inkblogdb.ddd.application.usecase.user;
+
+import com.inkblogdb.ddd.application.dto.user.UserDTO;
+import com.inkblogdb.ddd.domain.model.user.UserId;
+import com.inkblogdb.ddd.domain.model.user.UserName;
+import com.inkblogdb.ddd.domain.model.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AddUserUseCase {
+
+  private final UserService userService;
+
+  public UserDTO addUser(UserName userName) {
+    UserId userId = userService.addUser(userName);
+    UserDTO userDTO = new UserDTO();
+    userDTO.setId(userId.toString());
+    return userDTO;
+  }
+
+}

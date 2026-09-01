05a26f81b7789ef4e0c483aab2635348e9ce11b7
ink.0x20 <ink.0x20@gmail.com>
2026-09-02 00:11:19 +0900

    feat: ユーザーを追加するAPIを実装

--- src/main/java/com/inkblogdb/ddd/presenter/api/v1/UserController.java
+++ src/main/java/com/inkblogdb/ddd/presenter/api/v1/UserController.java
@@ -2,13 +2,12 @@ package com.inkblogdb.ddd.presenter.api.v1;
 
 import com.inkblogdb.ddd.application.dto.user.UserDTO;
 import com.inkblogdb.ddd.application.usecase.abort.UserNotFondException;
+import com.inkblogdb.ddd.application.usecase.user.AddUserUseCase;
 import com.inkblogdb.ddd.application.usecase.user.GetUserUseCase;
 import com.inkblogdb.ddd.domain.model.user.UserId;
+import com.inkblogdb.ddd.domain.model.user.UserName;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -16,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final GetUserUseCase getUserUseCase;
+  private final AddUserUseCase addUserUseCase;
 
   @GetMapping("/{userId}")
   public UserDTO getUser(@PathVariable String userId) throws UserNotFondException {
     return getUserUseCase.getUser(new UserId(userId));
+  }
+
+  @PutMapping
+  public UserDTO addUser(@RequestBody UserRequest userRequest) {
+    UserName userName = new UserName(userRequest.getName());
+    return addUserUseCase.addUser(userName);
   }
 
 }
---
+++ src/main/java/com/inkblogdb/ddd/presenter/api/v1/UserRequest.java
@@ -0,0 +1,12 @@
+package com.inkblogdb.ddd.presenter.api.v1;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class UserRequest {
+
+  private String name;
+
+}

7b65b5b1fad4ed76c5c39bf7f755fb55fe5c773a
ink.0x20 <ink.0x20@gmail.com>
2026-09-03 21:43:47 +0900

    refactor: RecordEntityをclassからrecordに変更

--- src/main/java/com/inkblogdb/ddd/infrastructure/database/user/UserDataSource.java
+++ src/main/java/com/inkblogdb/ddd/infrastructure/database/user/UserDataSource.java
@@ -18,8 +18,8 @@ public class UserDataSource implements UserRepository {
   public Optional<User> findById(UserId userId) {
     Optional<UserRecordEntity> userRecordEntity = Optional.ofNullable(userMapper.findById(userId.value()));
     return userRecordEntity.map(entity -> new User(
-        new UserId(entity.getId()),
-        new UserName(entity.getName())
+        new UserId(entity.id()),
+        new UserName(entity.name())
     ));
   }
 
--- src/main/java/com/inkblogdb/ddd/infrastructure/database/user/UserRecordEntity.java
+++ src/main/java/com/inkblogdb/ddd/infrastructure/database/user/UserRecordEntity.java
@@ -1,17 +1,7 @@
 package com.inkblogdb.ddd.infrastructure.database.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserRecordEntity {
-
-  private UUID id;
-  private String name;
+public record UserRecordEntity(UUID id, String name) {
 
 }
